dd99399e7072d5f435097fdfb7ec56094a77017d
Sifyoyo <mark_bao95@hotmail.com>
2022-05-30 15:55:28 +0200

    ay dawg

--- mark/mark.js
+++ mark/mark.js
@@ -1,21 +1,55 @@
 const background = document.querySelectorAll(".background")
+const paragraphWrapper = document.querySelectorAll(".txt_wrapper")
+const paragraph = document.querySelectorAll(".txt_with_fade")
 
 window.addEventListener(
     "load",
     (e) => {
-        const paragraph = document.querySelectorAll(".txt_observer")
 
-        observer.observe(paragraph[0]);
-        observer2.observe(paragraph[1]);
+        ObjectObserveToChangeBackground(0);
+        ObjectObserveToChangeBackground(1);
+
+        ObjectObserveToFadeText(0);
+        ObjectObserveToFadeText(1)
+        
     },
     false
   );
   
 
-const observer = new IntersectionObserver(Elentries =>{
-    background[0].classList.toggle("show-background", Elentries[0].isIntersecting)
-})
+const ObjectObserveToChangeBackground = (index) => {
 
-const observer2 = new IntersectionObserver(Elentries =>{
-    background[1].classList.toggle("show-background", Elentries[0].isIntersecting)
-})
+
+    const options = {
+        root: null,
+        rootMargin: "-33%",
+        threshold: 0.05,
+    }
+
+    const observer = new IntersectionObserver(Elentries =>{
+        background[index].classList.toggle("show-background", Elentries[0].isIntersecting)
+    }, 
+        options
+    )
+
+    return  observer.observe(paragraphWrapper[index]);
+}
+
+const ObjectObserveToFadeText = (index) => {
+
+
+    const options = {
+        root: null,
+        rootMargin: "-10% 0px 200px 0px",
+        threshold: 0.80,
+    }
+
+    const observer = new IntersectionObserver(Elentries =>{
+        paragraph[index].classList.toggle("hideTxt", !Elentries[0].isIntersecting)
+        console.log(Elentries);
+    }, 
+        options
+    )
+
+    return  observer.observe(paragraphWrapper[index]);
+}
